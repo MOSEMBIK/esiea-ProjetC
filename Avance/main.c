a0e1445de6d35e6b07c6c2e991a64735c096185e
DeLaSerreFR <80880462+DeLaSerreFR@users.noreply.github.com>
2022-11-11 14:45:52 +0100

    First commit v2

--- Avance/main.c
+++ Avance/main.c
@@ -1,1 +1,86 @@
-#include <stdio.h>;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_BUFFER_SIZE 1000
+
+struct char_struct
+{
+    int nombre;
+    unsigned long frequence;
+};
+
+struct char_struct *char_struct;
+
+
+int lire_caracteres(char nom_fichier[]){
+    int char_count = 0;
+    FILE *fPointer;
+    fPointer = fopen(nom_fichier, "r");
+    char singleLine[MAX_BUFFER_SIZE];
+
+    if(fPointer == NULL){
+        printf("Erreur fopen\n");
+        return 1;
+    }
+
+    char_struct = malloc(sizeof(struct char_struct));
+
+    while(!feof(fPointer)){
+        fgets(singleLine, MAX_BUFFER_SIZE, fPointer);
+        for(int i=0; i<strlen(singleLine); i++){
+            printf("%d\n", char_struct[singleLine[i]].nombre);
+            char_struct[singleLine[i]].nombre++;
+            char_count++;
+        }
+    }
+
+    printf("%d", char_count);
+
+    fclose(fPointer);
+    return char_count;
+}
+
+
+struct char_struct * generer_frequences(int char_count){
+    for(int i=0; i<sizeof(struct char_struct); i++){
+        char_struct[i].frequence = char_struct[i].nombre / char_count;
+    }
+    return char_struct;
+}
+
+
+void creer_arbre(){
+    return;
+}
+
+
+void encoder_frequences(){
+    return;
+}
+
+
+void associer_frequences_encodage(){
+    return;
+}
+
+
+void creer_nouveau_fichier(){
+    return;
+}
+
+
+void compresser_fichier(){
+    return;
+}
+
+
+void decompresser_fichier(){
+    return;
+}
+
+
+int main(){
+    lire_caracteres("./test.txt");
+    return 0;
+}
--- Intermediaire/main.c
+++ Intermediaire/main.c
@@ -1,1 +1,90 @@
-#include <stdio.h>;
+#include <stdio.h>
+#include <string.h>
+
+
+int input_reader(int nb_a_retirer, int max){
+    printf("Combien prenez-vous d'allumettes ? (entre 1 et %d compris)\n", max);
+    scanf("%d", &nb_a_retirer);
+    while(nb_a_retirer > max || nb_a_retirer < 1) {
+        printf("Erreur ! Prise invalide : %d\n", nb_a_retirer);
+        printf("Recommencez !\n");
+
+        fflush(stdin);
+        scanf("%d", &nb_a_retirer);
+    }
+    return nb_a_retirer;
+}
+
+
+int game(int nb_allumettes, char joueur_actuel[16]){
+    int nb_a_retirer = 0;
+    printf("Nombre d'allumettes restantes : %d\n", nb_allumettes);
+    for(int i=0; i<nb_allumettes; i++){
+        printf("| ");
+    }
+    printf("\nAu tour de %s\n", joueur_actuel);
+
+    if(nb_allumettes >= 3){
+        nb_a_retirer = input_reader(nb_a_retirer, 3);
+    }
+
+    if(nb_allumettes == 2){
+        nb_a_retirer = input_reader(nb_a_retirer, 2);
+    }
+
+    if(nb_allumettes == 1){
+        nb_a_retirer = 1;
+    }
+
+    printf("%s prend %d allumette(s)\n", joueur_actuel, nb_a_retirer);
+    printf("\n");
+    nb_allumettes = nb_allumettes - nb_a_retirer;
+
+    return nb_allumettes;
+}
+
+
+int main() {
+    char joueur1[16];
+    char joueur2[16];
+    char joueur_actuel[16];
+    int niveau = 1;
+    int nb_allumettes = 20;
+
+    printf("Nom du joueur 1 (16 caracteres autorises) :");
+    scanf("%s", joueur1);
+    fflush(stdin);
+    while(strlen(joueur1) > 16) {
+        printf("Erreur ! 16 caracteres autorises.\n");
+        printf("\nNom du joueur 1 :\n");
+        scanf("%s", joueur1);
+        fflush(stdin);
+    }
+
+    printf("Nom du joueur 2 (16 caracteres autorises) :");
+    scanf("%s", joueur2);
+    fflush(stdin);
+    while(strlen(joueur2) > 16) {
+        printf("Erreur ! 16 caracteres autorises.\n");
+        printf("\nNom du joueur 2 :\n");
+        scanf("%s", joueur2);
+        fflush(stdin);
+    }
+
+    printf("Jeu contre %s, niveau %d, avec %d allumettes au depart \n", joueur2, niveau, nb_allumettes);
+    printf("\n");
+
+    strncpy(joueur_actuel, joueur1, 16);
+    while(nb_allumettes != 0){
+        if(strcmp(joueur_actuel, joueur1) == 0){
+            nb_allumettes = game(nb_allumettes, joueur_actuel);
+            strncpy(joueur_actuel, joueur2, 16);
+        } else if(strcmp(joueur_actuel, joueur2) == 0){
+            nb_allumettes = game(nb_allumettes, joueur_actuel);
+            strncpy(joueur_actuel, joueur1, 16);
+        }
+    }
+
+    printf("FIN DE PARTIE\n");
+    printf("Le joueur %s a gagne !\n", joueur_actuel);
+}
