08254b06b5d084312d36d7cd892383d1635fc4f1
DeLaSerreFR <80880462+DeLaSerreFR@users.noreply.github.com>
2022-11-11 20:30:32 +0100

    Compression decompression

--- Avance/struct.c
+++ Avance/struct.c
@@ -68,10 +68,76 @@ void encoder_fichier(char void_char[], char nom_fichier[], codestruct code[]){
 
 
 void compresser_fichier(char fichier_output[], char encoded_char[]) {
-    
+    printf("\n\n COMPRESSING FILE...");
+    printf("\n\n 1.");
+    FILE *fPointer;
+    fPointer = fopen(fichier_output, "w");
+
+    printf("\n\n 2.");
+    if(fPointer == NULL){
+        printf("Erreur fopen\n");
+        return;
+    }
+
+    printf("\n\n 3.");
+    fprintf(fPointer, "%s", encoded_char);
+
+    printf("\n\n 4.");
+    fclose(fPointer);    
 }
 
 
 void decompresser_fichier(char fichier_encode[], char fichier_decode[], arbrestruct arbre_struct[]) {
+    printf("\n\n UNCOMPRESSING FILE...");
+    printf("\n\n 1.");
+    FILE *fPointer;
+    char singleLine[MAX_BUFFER_SIZE];
+    fPointer = fopen(fichier_encode, "r");
 
+    printf("\n\n 2.");
+    if(fPointer == NULL){
+        printf("Erreur fopen\n");
+        return;
+    }
+
+    printf("\n\n 3.");
+    int count = 0;
+    unsigned char output[] = {NULL};
+    arbrestruct tmp = arbre_struct[0];
+
+    while(!feof(fPointer)){
+        printf("\n\n 3.1");
+        fgets(singleLine, MAX_BUFFER_SIZE, fPointer);
+        printf("\n\n 3.2");
+        for(int i=0; i<strlen(singleLine); i++){
+            if(tmp.value.ascii_char == NULL){
+                if(singleLine[i] == 0){
+                    tmp = *tmp.a_gauche;
+                } else if(singleLine[i] == 1) {
+                    tmp = *tmp.a_droite;
+                }
+            } else {
+                output[count] = tmp.value.ascii_char;
+                count++;
+            }
+        }
+    }
+
+    printf("\n\n 5.");
+    fclose(fPointer);
+
+    printf("Writing part");
+    fPointer = fopen(fichier_decode, "w");
+
+    printf("\n\n 2.");
+    if(fPointer == NULL){
+        printf("Erreur fopen\n");
+        return;
+    }
+
+    printf("\n\n 3.");
+    fprintf(fPointer, "%s", output);
+
+    printf("\n\n 5.");
+    fclose(fPointer);
 }
