edd53aef7846284a2f0e4567178515b784687491
MOSEMBIK <contact@mosembik.fr>
2022-11-12 16:13:42 +0100

    End build tree

--- Avance/struct.c
+++ Avance/struct.c
@@ -10,18 +10,18 @@ int aStructComparator(arbrestruct * first, arbrestruct * second) {
 
 int lire_caracteres(charstruct char_struct[], char nom_fichier[]){
     printf("\n\n READING FILE...");
-    printf("\n\n 1.");
+    // printf("\n\n 1.");
     FILE *fPointer;
     fPointer = fopen(nom_fichier, "r");
     char singleLine[MAX_BUFFER_SIZE];
 
-    printf("\n\n 2.");
+    // printf("\n\n 2.");
     if(fPointer == NULL){
         printf("Erreur fopen\n");
         return -1;
     }
 
-    printf("\n\n 3.");
+    // printf("\n\n 3.");
     int char_idx = 0;
     int char_count = 0;
     charindexstruct arr[MAX_BUFFER_SIZE] = {NULL, 0};
@@ -38,9 +38,10 @@ int lire_caracteres(charstruct char_struct[], char nom_fichier[]){
                 char_struct[char_idx].ascii_char = singleLine[i];
             char_struct[char_idx].occurrence++;
         }
+        // printf("\n %d", strlen(singleLine));
     }
 
-    printf("\n\n 4.");
+    // printf("\n\n 4.");
     fclose(fPointer);
 
     return char_count;
@@ -52,7 +53,7 @@ void creer_arbre(arbrestruct arbre_struct[], charstruct char_struct[], int cst_s
     int arbre_with_occ = 0;
 
     // Construction des arbres
-    printf("\n\n 1.");
+    //printf("\n\n 1.");
     for (int i=0; i<cst_size; i++){
         arbre_struct[i].value.ascii_char = char_struct[i].ascii_char;
         arbre_struct[i].value.occurrence = char_struct[i].occurrence;
@@ -60,81 +61,54 @@ void creer_arbre(arbrestruct arbre_struct[], charstruct char_struct[], int cst_s
     }
 
     // Tri des arbre sur occurrence de value
-    printf("\n\n 2.");
-    qsort(arbre_struct, cst_size, sizeof(arbrestruct), aStructComparator);
-    printf("\t\n Size of char_struct : %d\n", cst_size);
-    for (int i=0; i<cst_size; i++){
-        if (arbre_struct[i].value.occurrence != 0){
-            printf("\n Index : %d \n Char : '%c' \n Occurence : %d\n", i, arbre_struct[i].value.ascii_char, arbre_struct[i].value.occurrence);
-            arbre_with_occ++;
-        }   
-    }
+    //printf("\n\n 2.");
+    // qsort(arbre_struct, cst_size, sizeof(arbrestruct), aStructComparator);
+    // for (int i=0; i<cst_size; i++){
+    //     if (arbre_struct[i].value.occurrence != 0){
+    //         printf("\n Index : %d \n Char : '%c' \n Occurence : %d\n", i, arbre_struct[i].value.ascii_char, arbre_struct[i].value.occurrence);
+    //         arbre_with_occ++;
+    //     }   
+    // }
 
     // Processing de la list arbre_struct
-    printf("\n\n 2.");
+    // printf("\n\n 2.");
     int cpt_a_idx = 0;
-    printf("\n\n 2.1");
+    // printf("\n\n 2.1");
+    int new_occ = 0;
+    int last_index = 0;
     while(cpt_a_idx < 256){
-        printf("\n\n 2.1.1");
+        //printf("\n\n 2.1.1");
         qsort(arbre_struct, (cst_size + (cpt_a_idx/2)), sizeof(arbrestruct), aStructComparator);
 
         //printf("\n\n 2.1.2");
         arbre_struct[cst_size+(cpt_a_idx/2)].value.ascii_char = NULL;
-        printf("\n Index 1 : %d", 0 + cpt_a_idx);
-        printf("\n Char G : '%c'", arbre_struct[0 + cpt_a_idx].value.ascii_char);
-        printf("\n Occ G : %d", arbre_struct[0 + cpt_a_idx].value.occurrence);
-        printf("\n Index 2 : %d", 1 + cpt_a_idx);
-        printf("\n Char D : '%c'", arbre_struct[1 + cpt_a_idx].value.ascii_char);
-        printf("\n Occ D : %d", arbre_struct[1 + cpt_a_idx].value.occurrence);
-        arbre_struct[cst_size+(cpt_a_idx/2)].value.occurrence = arbre_struct[0 + cpt_a_idx].value.occurrence + arbre_struct[1 + cpt_a_idx].value.occurrence;
-        arbre_struct[cst_size+(cpt_a_idx/2)].a_gauche = &arbre_struct[0 + cpt_a_idx];
-        arbre_struct[cst_size+(cpt_a_idx/2)].a_droite = &arbre_struct[1 + cpt_a_idx];
-        if(arbre_struct[cst_size+(cpt_a_idx/2)].value.occurrence != 0){
-            printf("\n\n Index : %d", cst_size+(cpt_a_idx/2));
-            printf("\n Global occ : %d", arbre_struct[cst_size+(cpt_a_idx/2)].value.occurrence);
-            printf("\n Char G : '%c'", arbre_struct[cst_size+(cpt_a_idx/2)].a_gauche->value.ascii_char);
-            printf("\n Occ G : %d", arbre_struct[cst_size+(cpt_a_idx/2)].a_gauche->value.occurrence);
-            printf("\n Char D : '%c'", arbre_struct[cst_size+(cpt_a_idx/2)].a_droite->value.ascii_char);
-            printf("\n Occ D : %d", arbre_struct[cst_size+(cpt_a_idx/2)].a_droite->value.occurrence);
+        new_occ = arbre_struct[0 + cpt_a_idx].value.occurrence + arbre_struct[1 + cpt_a_idx].value.occurrence;
+        if(new_occ != 0){
+            arbre_struct[cst_size+(cpt_a_idx/2)].value.occurrence = new_occ;
+            arbre_struct[cst_size+(cpt_a_idx/2)].a_gauche = &arbre_struct[0 + cpt_a_idx];
+            arbre_struct[cst_size+(cpt_a_idx/2)].a_droite = &arbre_struct[1 + cpt_a_idx];
         } else {
+            //printf("\n\n BREAK");
+            last_index = cst_size+(cpt_a_idx/2) - 1;
             break;
         }
         cpt_a_idx += 2;
     }
     
-    printf("\n\n 2.2");
-    // printf("\n Occ G : %d", arbre_struct[78].a_gauche->value.occurrence);
-    // printf(
-    //     "\n Index : %d\n Char : '%c'\n Occurence : %d\n Char Gauche : %c\n Occurrence Gauche : %d\n Char Droit : %c\n Occurrence Droit : %d", 
-    //     50, 
-    //     arbre_struct[50].value.ascii_char, 
-    //     arbre_struct[50].value.occurrence, 
-    //     arbre_struct[50].a_gauche->value.ascii_char, 
-    //     arbre_struct[50].a_gauche->value.occurrence, 
-    //     arbre_struct[50].a_droite->value.ascii_char, 
-    //     arbre_struct[50].a_droite->value.occurrence
-    // );
-    int last_index = 0;
-    for (int i=cst_size; i<cpt_a_idx; i++){
-        if (arbre_struct[i].value.occurrence != 0){
-            printf(
-                "\n\n Index : %d\n Char : '%c'\n Occurence : %d\n Char Gauche : '%c'\n Occurrence Gauche : %d\n Char Droit : '%c'\n Occurrence Droit : %d", 
-                i, 
-                arbre_struct[i].value.ascii_char, 
-                arbre_struct[i].value.occurrence, 
-                arbre_struct[i].a_gauche->value.ascii_char, 
-                arbre_struct[i].a_gauche->value.occurrence, 
-                arbre_struct[i].a_droite->value.ascii_char, 
-                arbre_struct[i].a_droite->value.occurrence
-            );
-            last_index = i-1;
-        }
-    }
+    printf("\n\n Built tree :");
     endcoded_arbre_struct = arbre_struct[last_index];
-
-    printf("\n Char : '%c' \n Occurence : %d\n", endcoded_arbre_struct.a_gauche->value.ascii_char, endcoded_arbre_struct.a_gauche->value.occurrence);
+    printf(
+        "\n Root Char : '%c'\n Root Occurence : %d\n\n Char Gauche : '%c'\n Occurrence Gauche : %d\n\n Char Droit : '%c'\n Occurrence Droit : %d",  
+        endcoded_arbre_struct.value.ascii_char, 
+        endcoded_arbre_struct.value.occurrence, 
+        endcoded_arbre_struct.a_gauche->value.ascii_char, 
+        endcoded_arbre_struct.a_gauche->value.occurrence, 
+        endcoded_arbre_struct.a_droite->value.ascii_char, 
+        endcoded_arbre_struct.a_droite->value.occurrence
+    );
 
     // Création du tableau de correspondances
+    
 
 }
 
